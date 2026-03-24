8441b7e6e49647e8b93427707d3cccd9f78d8219
Dhruvkumar Ghadiali <dhruvghadiali@hotmail.com>
2026-03-24 22:33:59 +0530

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Button} from "@MEShadcnComponents/button";
+
+function App() {
+
+  return (
+    <div className="mx-5 my-5">
+      <h1 className="text-9xl font-bold"> Hello !!!</h1>
+      <Button size="lg">Click me</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@MEShadcnComponents': path.resolve(__dirname, './src/components/ui')
+    },
+  },
+})
