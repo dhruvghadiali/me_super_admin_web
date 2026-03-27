fb53aa9360b1596170928bd6530b56a8dfc73529
Dhruvkumar Ghadiali <dhruvghadiali@hotmail.com>
2026-03-28 01:47:56 +0530

    Updated UI for sidebar menu

--- src/components/common/sidebar/index.jsx
+++ src/components/common/sidebar/index.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { LogOutIcon, LucideMessageCircleWarning } from "lucide-react";
 
 import _ from "lodash";
 
 import { menuItems } from "@MECommonComponents/sidebar/sidebarMenu";
-import { sidebarHeader, sidebarMenuTitle } from "@MELocalization/en";
+import { signOut } from "@MERedux/authentication/authenticationSlice";
 import {
   Sidebar,
   SidebarMenu,
@@ -19,18 +21,45 @@ import {
   SidebarGroupLabel,
   SidebarMenuButton,
   SidebarGroupContent,
+  SidebarFooter,
 } from "@MEShadcnComponents/sidebar";
+import {
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogMedia,
+  AlertDialogFooter,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogTrigger,
+  AlertDialogDescription,
+} from "@MEShadcnComponents/alert-dialog";
+import {
+  sidebarHeader,
+  sidebarMenuTitle,
+  sidebarSignoutTitle,
+  sidebarSignoutMenuLabel,
+  sidebarSignoutDescription,
+} from "@MELocalization/en";
 
 import logo from "@MEAssets/logo.png";
 
 const MESidebar = ({ children }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [activeMenuIndex, setActiveMenuIndex] = useState(0);
 
   const onSidebarMenuItemClick = (index, path) => {
     setActiveMenuIndex(index);
     navigate(path);
+  };
+
+  const handleSignout = () => {
+    dispatch(signOut());
+    setShowSignout(false);
+    navigate("/");
   };
 
   return (
@@ -68,6 +97,61 @@ const MESidebar = ({ children }) => {
             </SidebarGroupContent>
           </SidebarGroup>
         </SidebarContent>
+        <SidebarFooter>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton
+                    className={
+                      "hover:bg-secondary hover:text-primary hover:cursor-pointer"
+                    }
+                    onClick={() => setShowSignout(true)}
+                  >
+                    <LogOutIcon />
+                    <span>
+                      {_.upperFirst(
+                        t("sidebarSignoutMenuLabel", {
+                          defaultValue: sidebarSignoutMenuLabel,
+                        }),
+                      )}
+                    </span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogMedia className={"bg-transparent"}>
+                  <LucideMessageCircleWarning className="text-destructive" />
+                </AlertDialogMedia>
+                <AlertDialogTitle>
+                  {_.upperFirst(
+                    t("sidebarSignoutTitle", {
+                      defaultValue: sidebarSignoutTitle,
+                    }),
+                  )}
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                  {_.upperFirst(
+                    t("sidebarSignoutDescription", {
+                      defaultValue: sidebarSignoutDescription,
+                    }),
+                  )}
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  variant="destructive"
+                  onClick={handleSignout}
+                >
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </SidebarFooter>
       </Sidebar>
       <SidebarInset>
         <header className="flex h-12 items-center gap-2 border-b px-4">
--- src/localization/component/sidebar/sidebarTranslationEn.jsx
+++ src/localization/component/sidebar/sidebarTranslationEn.jsx
@@ -1,4 +1,13 @@
-const sidebarHeader = "super admin";
 const sidebarMenuTitle = "menu";
+const sidebarHeader = "super admin";
+const sidebarSignoutMenuLabel = "sign out";
+const sidebarSignoutTitle = "are you absolutely sure?";
+const sidebarSignoutDescription = `you will be signed out of your account and will need to sign in again to access the dashboard.`;
 
-export { sidebarHeader, sidebarMenuTitle };
+export {
+  sidebarHeader,
+  sidebarMenuTitle,
+  sidebarSignoutTitle,
+  sidebarSignoutMenuLabel,
+  sidebarSignoutDescription,
+};
